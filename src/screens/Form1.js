279dff4831547215eacc051a1bb2865c410d547b
posadari <57573681+posadari@users.noreply.github.com>
2021-04-04 14:09:51 -0500

    work on form1 and button

--- src/screens/Form1.js
+++ src/screens/Form1.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React from 'react';
 import NavBar from '../components/navbar'
 import '../styles/Form1.css'
-const Form1 = () => {
-    return(
-        <div>
+import Form from 'react-bootstrap/Form'
+import Dropdown from 'react-bootstrap/Dropdown'
+import { Link } from "wouter";
+import { ROUTES } from '../const/routes'
+import '../styles/button.css'
+
+const Form1 = (props) => {
+  return (
+      <div className="form1">
             <NavBar />
             <h2>Fill out the information below to get matched with your future pet!</h2>
+            <div className="container">
+                <Form.Group className="user-location">
+                    <Form.Control placeholder="Enter Zip Code"/>
+                </Form.Group>
+                <Dropdown>
+                    <Dropdown.Toggle variant="Warning" className="input-dropdown" id="dropdown-basic">
+                        Distance
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item className="input-dropdown-item" onClick={() => console.log("clicke")}>10 mi</Dropdown.Item>
+                        <Dropdown.Item className="input-dropdown-item" onClick={() => console.log("clicke")}>20 mi</Dropdown.Item>
+                        <Dropdown.Item className="input-dropdown-item" onClick={() => console.log("clicke")}>50 mi</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Link to={ROUTES.FORM2}><button class="button">CONTINUE</button></Link>
         </div>
-    )
+    </div>
+  );
 }
-
 export default Form1;
