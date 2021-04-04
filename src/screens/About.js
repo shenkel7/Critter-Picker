4bd438897265431c1947eca9fcb38047b533f14e
posadari <57573681+posadari@users.noreply.github.com>
2021-04-04 17:08:36 -0500

    add about page

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,9 +1,55 @@
 import React from 'react'
+import '../styles/about.css'
+import NavBar from '../components/navbar'
+import img from '../images/aboutme.svg'
+import img2 from '../images/website.svg'
 
-const About = () => {
+function OnePerson(props) {
     return(
-        <div>About</div>
-    )
+        <div>
+        <div className="card text-center">
+            <div className="overflow">
+                { <img src={props.imgsrc} alt="process" className="card-img-top"></img> }
+            </div>
+            <div className="card-body text-dark">
+                <h4 className="card-title">{props.title}</h4>
+                <p className="card-text text-secondary">
+                {props.desc}
+                </p>
+            </div>
+        </div>
+        </div>
+    );
 }
 
-export default About;
+function TwoPeople() {
+    return (
+        <div>
+            <NavBar />
+            <div class="header">
+                <div class="box">
+                    <h1></h1>
+                </div>
+            </div>
+            <div className="purpose">
+            <br></br>
+            <h1>PURPOSE</h1>
+            <div className="desc"><p>We wanted to make a website where people can easily find matches for adopting pets!</p>
+            </div>
+        </div>
+         <h1>CREATORS</h1>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="arielle">
+                        <OnePerson imgsrc = {img} title="Arielle Posadas" desc="CS Major at UTD"/>
+                    </div>
+                    <div className="kelly">
+                        <OnePerson imgsrc = {img2} title="Kelly Shen" desc="CS Major at UT"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    ) 
+}
+
+export default TwoPeople;
