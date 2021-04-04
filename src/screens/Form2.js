711efcb1ad6431e6400b900a1e521b683e80fc83
Kelly Shen <kellyshenfrog@gmail.com>
2021-04-04 10:41:20 -0500

    env, api call working

--- src/screens/Form2.js
+++ src/screens/Form2.js
@@ -1,6 +1,26 @@
 import React from 'react'
 
+const PET_FINDER_URL = 'https://api.petfinder.com';
+
 const Form2 = () => {
+
+    const getToken = () => {
+        fetch(`${PET_FINDER_URL}/v2/oauth2/token?grant_type=client_credentials&client_id=${process.env.REACT_APP_PETFINDER_KEY}&client_secret=${process.env.REACT_APP_PETFINDER_SECRET}`, { 
+            method: 'get', 
+          }).then(response => console.log(response));
+    }
+
+    const doApiCall = () => {
+        fetch(`${PET_FINDER_URL}/v2/animals?location=77386&distance=25`, { 
+          method: 'get', 
+          headers: new Headers({
+            Authorization: `Bearer ${process.env.REACT_APP_PETFINDER_TOKEN}`, 
+          }), 
+        }).then(response => response.json())
+        .then(data =>{
+            console.log(data);
+        });
+    }
     return(
         <div>Form2</div>
     )
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -25,4 +25,6 @@ function Home() {
   )
 }
 
+
+
 export default Home;
