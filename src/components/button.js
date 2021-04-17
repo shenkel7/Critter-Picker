f5ff3de7e0b100fd171eff29425eb9e548c5cb63
posadari <57573681+posadari@users.noreply.github.com>
2021-04-16 21:01:40 -0500

    added new buttons

--- src/components/button.js
+++ src/components/button.js
@@ -17,6 +17,11 @@ const Buttons = () => {
             <div class="row">
                 <div class="column"><button class={type == "Dog" ? "button" : "button-no"} onClick={()=> changeType("Dog")}>Dog</button></div>
                 <div class="column"><button class={type == "Cat" ? "button" : "button-no"} onClick={()=> changeType("Cat")}>Cat</button></div>
+                <div class="column"><button class={type == "Rabbit" ? "button" : "button-no"} onClick={()=> changeType("Rabbit")}>Rabbit</button></div>
+                <div class="column"><button class={type == "Scales & Fins" ? "button" : "button-no"} onClick={()=> changeType("Scales & Fins")}>Fish</button></div>
+                <div class="column"><button class={type == "Birds" ? "button" : "button-no"} onClick={()=> changeType("Birds")}>Birds</button></div>
+                <div class="column"><button class={type == "Horses" ? "button" : "button-no"} onClick={()=> changeType("Horses")}>Horses</button></div>
+                <div class="column"><button class={type == "Barnyard" ? "button" : "button-no"} onClick={()=> changeType("Barnyard")}>Barnyard</button></div>
                 <div class="column"><button class={type == "Small & Furry" ? "button" : "button-no"} onClick={()=> changeType("Small & Furry")}>Other</button></div>
             </div>
         ) 
